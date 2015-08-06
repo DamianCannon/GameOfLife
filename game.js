341c7de6300a0f36670ceadab6027551547c014d
Damian Cannon <damianbcannon@hotmail.com>
2015-08-06 18:32:28 +0100

    Add tests for moving grid to next state

--- game.js
+++ game.js
@@ -9,6 +9,7 @@
    4 - Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
 */
 /* jshint -W117 */
+/* jshint -W086 */
 if (typeof Game === 'undefined') {
     Game = {};
 }
@@ -23,5 +24,59 @@ Game.model = {
         });
         
         return liveNeighbours.length;
+    },
+    
+    newStateForCell: function(cell) {
+        var countOfLiveNeighbours = this.countNeighbours(cell);
+        
+        switch(countOfLiveNeighbours) {
+            case 2:
+                if (cell.state) {
+                    return true;
+                }
+                else {
+                    return false;
+                }
+            case 3:
+                return true;
+            default:
+                return false;
+        }
+    },
+    
+    newStateForGrid: function(grid) {
+        var result = [];
+        
+        for (var row = 0; row < grid.length; row++) {
+            var newRow = [];
+
+            for (var col = 0; col < grid[row].length; col++) {
+                // get current cell
+                var currentCell = {};
+                currentCell.state = grid[row][col];
+                currentCell.neighbours = [];
+                
+                // get neighbours for cell in array
+                for (var innerRow = row - 1; innerRow <= row + 1; innerRow++) {
+                    if (grid[innerRow] !== undefined) {
+                        for (var innerCol = col - 1; innerCol <= col + 1; innerCol++) {
+                            if (grid[innerRow][innerCol] !== undefined && (innerRow !== row || innerCol !== col)) {
+                                currentCell.neighbours.push(grid[innerRow][innerCol]);
+                            }
+                        }
+                    }
+                }
+                
+                // get new state for cell
+                var newState = this.newStateForCell(currentCell);
+                
+                // set state of cell in output
+                newRow.push(newState);
+            }
+            
+            result.push(newRow);
+        }
+        
+        return result;
     }
 };
--- game.spec.js
+++ game.spec.js
@@ -45,6 +45,196 @@ describe('Life game', function() {
             expect(model.countNeighbours(cell)).toBe(0);
         });
         
+        it('should return dead state when live cell has no live neighbours', function() {
+            var cell = {state: true, neighbours: []};
+            expect(model.newStateForCell(cell)).toBe(false);
+        });
+
+        it('should return dead state when live cell has one live neighbour', function() {
+            var cell = {state: true, neighbours: [true]};
+            expect(model.newStateForCell(cell)).toBe(false);
+        });
+        
+        it('should return live state when live cell has two live neighbours', function() {
+            var cell = {state: true, neighbours: [true, true]};
+            expect(model.newStateForCell(cell)).toBe(true);
+        });
+        
+        it('should return live state when live cell has three live neighbours', function() {
+            var cell = {state: true, neighbours: [true, true, true]};
+            expect(model.newStateForCell(cell)).toBe(true);
+        });
+        
+        it('should return dead state when live cell has four live neighbours', function() {
+            var cell = {state: true, neighbours: [true, true, true, true]};
+            expect(model.newStateForCell(cell)).toBe(false);
+        });
+
+        it('should return live state when live cell has four dead and three live neighbours', function()               {
+            var cell = {state: true, neighbours: [false, true, false, true, false, true, false]};
+            expect(model.newStateForCell(cell)).toBe(true);
+        });
+
+        it('should return dead state when live cell has eight live neighbours', function() {
+            var cell = {state: true, neighbours: [true, true, true, true, true, true, true, true]};
+            expect(model.newStateForCell(cell)).toBe(false);
+        });
+        
+        it('should return dead state when live cell has eight dead neighbours', function() {
+            var cell = {state: true, neighbours: [false, false, false, false, false, false, false, false]};
+            expect(model.newStateForCell(cell)).toBe(false);
+        });
+        
+        it('should return dead state when dead cell has no live neighbours', function() {
+            var cell = {state: false, neighbours: []};
+            expect(model.newStateForCell(cell)).toBe(false);
+        });
+
+        it('should return dead state when dead cell has one live neighbour', function() {
+            var cell = {state: false, neighbours: [true]};
+            expect(model.newStateForCell(cell)).toBe(false);
+        });
+        
+        it('should return dead state when dead cell has two live neighbours', function() {
+            var cell = {state: false, neighbours: [true, true]};
+            expect(model.newStateForCell(cell)).toBe(false);
+        });
+        
+        it('should return live state when dead cell has three live neighbours', function() {
+            var cell = {state: false, neighbours: [true, true, true]};
+            expect(model.newStateForCell(cell)).toBe(true);
+        });
+        
+        it('should return same pattern when given a block still-life', function() {
+            var inputGrid = [
+               [false, false, false, false],
+               [false, true, true, false],
+               [false, true, true, false],
+               [false, false, false, false]
+            ];
+
+            var outputGrid = model.newStateForGrid(inputGrid);
+            
+            // row 1
+            expect(outputGrid[0][0]).toBe(false);
+            expect(outputGrid[0][1]).toBe(false);
+            expect(outputGrid[0][2]).toBe(false);
+            expect(outputGrid[0][3]).toBe(false);
+
+            // row 2
+            expect(outputGrid[1][0]).toBe(false);
+            expect(outputGrid[1][1]).toBe(true);
+            expect(outputGrid[1][2]).toBe(true);
+            expect(outputGrid[1][3]).toBe(false);
+
+            // row 3
+            expect(outputGrid[2][0]).toBe(false);
+            expect(outputGrid[2][1]).toBe(true);
+            expect(outputGrid[2][2]).toBe(true);
+            expect(outputGrid[2][3]).toBe(false);
+
+            // row 4
+            expect(outputGrid[3][0]).toBe(false);
+            expect(outputGrid[3][1]).toBe(false);
+            expect(outputGrid[3][2]).toBe(false);
+            expect(outputGrid[3][3]).toBe(false);        
+        });
+        
+        it('should return next pattern when given a blinker oscillator', function() {
+            var inputGrid = [
+               [false, false, false, false, false],
+               [false, false, false, false, false],
+               [false, true, true, true, false],
+               [false, false, false, false, false],
+               [false, false, false, false, false]
+            ];
+
+            var outputGrid = model.newStateForGrid(inputGrid);
+            
+            // row 1
+            expect(outputGrid[0][0]).toBe(false);
+            expect(outputGrid[0][1]).toBe(false);
+            expect(outputGrid[0][2]).toBe(false);
+            expect(outputGrid[0][3]).toBe(false);
+            expect(outputGrid[0][4]).toBe(false);
+
+            // row 2
+            expect(outputGrid[1][0]).toBe(false);
+            expect(outputGrid[1][1]).toBe(false);
+            expect(outputGrid[1][2]).toBe(true);
+            expect(outputGrid[1][3]).toBe(false);
+            expect(outputGrid[1][4]).toBe(false);
+
+            // row 3
+            expect(outputGrid[2][0]).toBe(false);
+            expect(outputGrid[2][1]).toBe(false);
+            expect(outputGrid[2][2]).toBe(true);
+            expect(outputGrid[2][3]).toBe(false);
+            expect(outputGrid[2][4]).toBe(false);
+
+            // row 4
+            expect(outputGrid[3][0]).toBe(false);
+            expect(outputGrid[3][1]).toBe(false);
+            expect(outputGrid[3][2]).toBe(true);
+            expect(outputGrid[3][3]).toBe(false);
+            expect(outputGrid[3][4]).toBe(false);
+
+            // row 5
+            expect(outputGrid[4][0]).toBe(false);
+            expect(outputGrid[4][1]).toBe(false);
+            expect(outputGrid[4][2]).toBe(false);
+            expect(outputGrid[4][3]).toBe(false);
+            expect(outputGrid[4][4]).toBe(false);
+        });
+        
+        it('should return to original pattern when oscillating a blinker twice', function() {
+            var inputGrid = [
+               [false, false, false, false, false],
+               [false, false, false, false, false],
+               [false, true, true, true, false],
+               [false, false, false, false, false],
+               [false, false, false, false, false]
+            ];
+
+            var intermediateGrid = model.newStateForGrid(inputGrid);
+            var outputGrid = model.newStateForGrid(intermediateGrid);
+            
+            // row 1
+            expect(outputGrid[0][0]).toBe(false);
+            expect(outputGrid[0][1]).toBe(false);
+            expect(outputGrid[0][2]).toBe(false);
+            expect(outputGrid[0][3]).toBe(false);
+            expect(outputGrid[0][4]).toBe(false);
+
+            // row 2
+            expect(outputGrid[1][0]).toBe(false);
+            expect(outputGrid[1][1]).toBe(false);
+            expect(outputGrid[1][2]).toBe(false);
+            expect(outputGrid[1][3]).toBe(false);
+            expect(outputGrid[1][4]).toBe(false);
+
+            // row 3
+            expect(outputGrid[2][0]).toBe(false);
+            expect(outputGrid[2][1]).toBe(true);
+            expect(outputGrid[2][2]).toBe(true);
+            expect(outputGrid[2][3]).toBe(true);
+            expect(outputGrid[2][4]).toBe(false);
+
+            // row 4
+            expect(outputGrid[3][0]).toBe(false);
+            expect(outputGrid[3][1]).toBe(false);
+            expect(outputGrid[3][2]).toBe(false);
+            expect(outputGrid[3][3]).toBe(false);
+            expect(outputGrid[3][4]).toBe(false);
+
+            // row 5
+            expect(outputGrid[4][0]).toBe(false);
+            expect(outputGrid[4][1]).toBe(false);
+            expect(outputGrid[4][2]).toBe(false);
+            expect(outputGrid[4][3]).toBe(false);
+            expect(outputGrid[4][4]).toBe(false);
+        });
+        
     });
     
 });
